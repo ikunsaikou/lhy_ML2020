5b9a7d336aecaa8f5456e87fa93831e953b4adae
nikuliu <nikuliu@foxmail.com>
2021-10-17 19:28:22 +0800

    对率回归

--- homework02/classification.py
+++ homework02/classification.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 X_train_fpath = './dataset/X_train'
 Y_train_fpath = './dataset/Y_train'
 X_test_fpath = './dataset/X_test'
-output_fpath = './output_{}.csv'
+output_fpath = './output/output_{}.csv'
 
 # Parse csv files to numpy array
 with open(X_train_fpath) as f:
@@ -18,8 +19,8 @@ with open(X_test_fpath) as f:
     X_test = np.array([line.strip('\n').split(',')[1:] for line in f], dtype=float)
 
 print(X_train)
-print(X_train.shape[0])
-print(X_train.shape[1])
+print(X_test.shape[0])
+print(X_test.shape[1])
 
 
 def _normalize(X, train=True, specified_column=None, X_mean=None, X_std=None):
@@ -41,7 +42,7 @@ def _normalize(X, train=True, specified_column=None, X_mean=None, X_std=None):
         specified_column = np.arange(X.shape[1])  # shape[1]代表矩阵的列数 = 510
     if train:
         X_mean = np.mean(X[:, specified_column], 0).reshape(1,
-                                                            -1)  # 将x的所有行和特定列传入，axis = 0 对个列求均值 reshape(1,-1)转换为行数1, 列数根据行数计算
+                                                            -1)  # 将x的所有行和特定列传入，axis = 0 对个列求均值 reshape(1,-1)转换为行数1, 列数根据X数据列数计算（1*510）
         X_std = np.std(X[:, specified_column], 0).reshape(1, -1)
 
     X[:, specified_column] = (X[:, specified_column] - X_mean) / (X_std + 1e-8)  # (x-μ)/σ  X_std加入一个很小的数防止分母除以0
@@ -62,11 +63,20 @@ X_test, _, _ = _normalize(X_test, train=False, X_mean=X_mean, X_std=X_std)  # '_
 dev_ratio = 0.1
 X_train, Y_train, X_dev, Y_dev = _train_dev_split(X_train, Y_train, dev_ratio=dev_ratio)
 
+train_size = X_train.shape[0]
+dev_size = X_dev.shape[0]
+test_size = X_test.shape[0]
+data_dim = X_train.shape[1]
+print('Size of training set: {}'.format(train_size))
+print('Size of development set: {}'.format(dev_size))
+print('Size of testing set: {}'.format(test_size))
+print('Dimension of data: {}'.format(data_dim))
+
 
 def _shuffle(X, Y):
     # 该函数对两个等长的列表或数组洗牌(XY同步).
     randomize = np.arange(len(X))  # arange(len(X))返回0,1...len(X)
-    np.random.shuffle(randomize)  # shuffle()方法将序列的所有元素随机排序，打乱randomize中的元素
+    np.random.shuffle(randomize)  # shuffle()方法将行的随机排序，打乱randomize中的元素
     return (X[randomize], Y[randomize])
 
 
@@ -90,7 +100,7 @@ def _f(X, w, b):
 
 def _predict(X, w, b):
     # 通过把对率回归函数的结果四舍五入(round), 并转换类型, 得到每行X真正的预测值.
-    return np.round(_f(X, w, b)).astype(np.int)
+    return np.round(_f(X, w, b)).astype(int)
 
 
 def _accuracy(Y_pred, Y_label):
@@ -118,3 +128,91 @@ def _gradient(X, Y_label, w, b):
     w_grad = -np.sum(pred_error * X.T, 1)  # sum参数axis=1是压缩列,即将每一行的元素相加,将矩阵压缩为一列
     b_grad = -np.sum(pred_error)
     return w_grad, b_grad
+
+
+# 将weights和bias初始化为0
+w = np.zeros((data_dim,))  # zeros第一个参数为形状
+b = np.zeros((1,))
+
+# 训练超参数
+max_iter = 272
+batch_size = 128
+learning_rate = 0.1
+regularization_weight = 0.005
+
+# 保存每一次迭代的损失与精确度, 用来画图
+train_loss = []
+dev_loss = []
+train_acc = []
+dev_acc = []
+
+# 计参数更新次数(步数)
+step = 1
+
+# 迭代式训练
+for epoch in range(max_iter):
+    # 每一代(epoch)随机洗牌
+    X_train, Y_train = _shuffle(X_train, Y_train)
+
+    # 小批训练
+    for idx in range(int(np.floor(train_size / batch_size))):  # np.floor()返回不大于输入参数的最大整数。（向下取整）
+        # 取这一小批的X, Y
+        X = X_train[idx * batch_size:(idx + 1) * batch_size]
+        Y = Y_train[idx * batch_size:(idx + 1) * batch_size]
+
+        # 计算梯度
+        w_grad, b_grad = _gradient(X, Y, w, b)
+
+        # 利用梯度下降更新参数wb
+        # 学习率随时间(step)减少
+        w = w * (1 - (learning_rate / np.sqrt(step)) * regularization_weight) - learning_rate / np.sqrt(step) * w_grad
+        b = b - learning_rate / np.sqrt(step) * b_grad
+
+        step = step + 1
+
+    # 计算训练集与发展集的损失与精确度
+    y_train_pred = _f(X_train, w, b)
+    Y_train_pred = np.round(y_train_pred)
+    train_acc.append(_accuracy(Y_train_pred, Y_train))
+    train_loss.append(_cross_entropy_loss(y_train_pred, Y_train) / train_size)  # / train_size消除训练集与发展集大小不同带来的影响
+
+    y_dev_pred = _f(X_dev, w, b)
+    Y_dev_pred = np.round(y_dev_pred)
+    dev_acc.append(_accuracy(Y_dev_pred, Y_dev))
+    dev_loss.append(_cross_entropy_loss(y_dev_pred, Y_dev) / dev_size)
+
+print('Training loss: {}'.format(train_loss[-1]))  # [-1]表示数组中最后一位
+print('Development loss: {}'.format(dev_loss[-1]))
+print('Training accuracy: {}'.format(train_acc[-1]))
+print('Development accuracy: {}'.format(dev_acc[-1]))
+
+# Loss curve
+plt.plot(train_loss)  # x可省略,默认[0,1..,N-1]递增
+plt.plot(dev_loss)
+plt.title('Loss')
+plt.legend(['train', 'dev'])  # 默认参数: 图例的名称
+plt.savefig('./output/loss.png')
+plt.show()
+
+# Accuracy curve
+plt.plot(train_acc)
+plt.plot(dev_acc)
+plt.title('Accuracy')
+plt.legend(['train', 'dev'])
+plt.savefig('acc.png')
+plt.show()
+
+# 预测测试集的标记
+predictions = _predict(X_test, w, b)
+with open(output_fpath.format('logistic'), 'w') as f:
+    f.write('id,label\n')
+    for i, label in enumerate(predictions):  # enumerate列出数据和生成数据下标
+        f.write('{},{}\n'.format(i, label))
+
+# 筛选最大(显著)的几个weight, 即得到最有用的特征
+ind = np.argsort(np.abs(w))[::-1]  # argsort从小到大排序, ::-1从后向前读取
+with open(X_test_fpath) as f:
+    content = f.readline().strip('\n').split(',')
+features = np.array(content)
+for i in ind[0:10]:
+    print(features[i], w[i])
